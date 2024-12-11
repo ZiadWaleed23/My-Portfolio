0c0f2c9a46116c7eb172b7b4bb10ae82e5bb032f
Ziad Waleed <164805169+ZiadWaleed23@users.noreply.github.com>
2024-12-12 00:41:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Get the button
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+// Show the button when the user scrolls down 300px from the top of the page
+window.onscroll = function() {
+if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+scrollTopBtn.style.display = "block";
+} else {
+scrollTopBtn.style.display = "none";
+}
+};
+
+// Scroll to the top of the page when the user clicks the button
+function scrollToTop() {
+window.scrollTo({
+top: 0,
+behavior: 'smooth'
+});
+}
